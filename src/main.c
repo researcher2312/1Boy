a707d16d899340228e3efa1170ff6c0c0118e959
Miłosz Segedyn <researcher2312@gmail.com>
2020-01-11 13:51:42 +0100

    changed frequency of interrupt to 1KHz

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include <avr/interrupt.h>
 #include "1Boy.h"
 
-volatile uint8_t time_cs = 0; //1/100 of second
+volatile int time_ms = 0;
 
 int main(){
 	uint8_t leds[] = {LED1, LED2, LED3, LED4, LED5, LED6, LED7, LED8};
@@ -13,17 +13,19 @@ int main(){
 	PORTD |= BT1 | BT2 | LED_RD | LED_GR | LED_BL;
 
 	TCCR0A |= (1<<WGM01); //CTC mode
-	TCCR0B |= (1<<CS00) | (1<<CS02); //1024 prescaler, timer on
-	OCR0A = 77; //delay, 100.16 Hz
+	TCCR0B |= (1<<CS00) | (1<<CS01); //64 prescaler, timer on
+	OCR0A = 125; //delay, 1 KHz
 	TIMSK |= (1<<OCIE0A); //interrupt enable
 
 	sei();
-	while(1);
+
+	while(1){
+
+	}
 
 
 }
 
 ISR(TIMER0_COMPA_vect){
-	++time_cs;
-
+	++time_ms;
 }
